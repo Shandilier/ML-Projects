fc50d7ff25b734e5b66194470cc5b842c808ac2e
Sachin Shandilya <31800224+Shandilier@users.noreply.github.com>
2018-06-19 19:48:55 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -5,7 +5,20 @@ Created on Tue Jun 19 21:32:26 2018
 
 @author: sachin1006
 """
+This is a problem based on diabetic condition  of the pregnent women whether the women will face diabetes or not.
+The dataset contains the following features.
 
+    Pregnancies: Number of times pregnant
+    Glucose: Plasma glucose concentration a 2 hours in an oral glucose tolerance test
+    BloodPressure: Diastolic blood pressure (mm Hg)
+    SkinThickness: Triceps skin fold thickness (mm)
+    Insulin: 2-Hour serum insulin (mu U/ml)
+    BMI: Body mass index (weight in kg/(height in m)^2)
+    DiabetesPedigreeFunction: Diabetes pedigree function
+    Age: Age (years)
+    Outcome: Class variable (0 or 1)
+
+"""
 # Import the packages.
 import numpy as np
 import pandas as pd
